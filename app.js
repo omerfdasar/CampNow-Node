237d865a86b32b0271575cebb4398054e6588dc7
Omer <omerfarukdasar@gmail.com>
2022-02-15 20:17:49 +0100

    before

--- app.js
+++ app.js
@@ -6,16 +6,29 @@ let howMany = document.getElementById("chances");
 let theNumber = Math.trunc(Math.random() * 99 + 1);
 let times = 7;
 let order = 1;
+
+function sorting(x) {
+  return x.sort(function (a, b) {
+    return a - b;
+  });
+}
+
 button.addEventListener("click", () => {
   let inputValue = inputbox.value;
+  let smallNums = [];
+  let bigNums = [];
   if (inputValue > theNumber) {
     times--;
+    bigNums.push(inputValue);
+    sorting(bigNums);
     display.innerText += `${order}) You Entered ${inputValue} => Enter a number between ${inputValue} and 0`;
     let brTags = document.createElement("br");
     display.appendChild(brTags);
     howMany.innerText = times;
   } else if (inputValue < theNumber) {
     times--;
+    smallNums.push(inputValue);
+    sorting(smallNums);
     display.innerText += `${order}) You Entered ${inputValue} => Enter a number between ${inputValue} and 100`;
     let brTags = document.createElement("br");
     display.appendChild(brTags);
@@ -26,5 +39,15 @@ button.addEventListener("click", () => {
     display.appendChild(brTags);
     howMany.innerText = times;
   }
+
+
+
+
+
+
+
+
+
+  
   order++;
 });
