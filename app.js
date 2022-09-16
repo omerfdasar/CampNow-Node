d4bbf8328f369300c691003efc4226fb91df3633
Omer Dasar <omerfarukdasar@gmail.com>
2022-09-16 23:06:22 +0200

    setup

--- app.js
+++ app.js
@@ -1,55 +1,34 @@
-let inputbox = document.getElementById("input");
-const button = document.getElementById("button");
-let display = document.getElementById("result");
-let remaining = document.getElementById("remaining");
-let howMany = document.getElementById("chances");
-let theNumber = Math.trunc(Math.random() * 99 + 1);
-let times = 7;
-let order = 1;
-
-function sorting(x) {
-  return x.sort(function (a, b) {
-    return a - b;
-  });
-}
-let smallNums = [0];
-let bigNums = [100];
-button.addEventListener("click", () => {
-  let inputValue = inputbox.value;
-
-  if (inputValue > theNumber) {
-    times--;
-    bigNums.push(+inputValue);
-    sorting(bigNums);
-    display.innerText += `${order}) You Entered ${inputValue} => Enter a number between ${+smallNums[
-      smallNums.length - 1
-    ]} and ${+bigNums[0]}`;
-    console.log(smallNums, bigNums);
-    console.log(smallNums[smallNums.length - 1], bigNums[0]);
-    let brTags = document.createElement("br");
-    display.appendChild(brTags);
-    howMany.innerText = times;
-  } else if (inputValue < theNumber) {
-    times--;
-    smallNums.push(+inputValue);
-    sorting(smallNums);
-    display.innerText += `${order}) You Entered ${inputValue} => Enter a number between ${+smallNums[
-      smallNums.length - 1
-    ]} and ${+bigNums[0]}`;
-
-    console.log(smallNums, bigNums);
-    console.log(smallNums[smallNums.length - 1], bigNums[0]);
-    let brTags = document.createElement("br");
-    display.appendChild(brTags);
-    howMany.innerText = times;
-  } else if ((inputValue = theNumber)) {
-    display.innerText += `${order}) You Entered ${inputValue} => You Guessed right`;
-    let brTags = document.createElement("br");
-    display.appendChild(brTags);
-    howMany.innerText = times;
-  }
-  inputbox.value = "";
-  order++;
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// return the value when fail
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "Cheap Camping",
+  });
+  await camp.save();
+  res.send("camp");
+});
+
+app.listen(3000, () => {
+  console.log("Serving from port");
+});
