e4ac3db883ebbf97d88ca2476eb288bc6dc42a8a
Omer Dasar <omerfarukdasar@gmail.com>
2022-09-21 13:23:18 +0200

    adding schema

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Campground = require("./models/campground");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const catchAsync = require("./utils/catchAsync");
-const Joi = require("joi");
+const {campgroundSchema} = require("./schemas")
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,6 +26,19 @@ app.set("views", path.join(__dirname, "views"));
 // for parsing request body
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+const validateCampground = (req, res, next) => {
+
+  const { error } = campgroundSchema.validate(req.body);
+
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -44,22 +57,8 @@ app.get("/campgrounds/new", (req, res) => {
 
 app.post(
   "/campgrounds",
+  validateCampground,
   catchAsync(async (req, res, next) => {
-    const campgroundSchema = Joi.object({
-      campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.number().required(),
-        location: Joi.number().required(),
-        description: Joi.number().required(),
-      }).required(),
-    });
-    const { error } = campgroundSchema.validate(req.body);
-
-    if (error) {
-      const msg = error.details.map((el) => el.message).join(",");
-      throw new ExpressError(msg, 400);
-    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -84,6 +83,7 @@ app.get(
 
 app.put(
   "/campgrounds/:id",
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
