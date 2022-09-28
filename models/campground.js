7fb916e62deba82908428cec6c276f6c1a1b31a2
Omer Dasar <omerfarukdasar@gmail.com>
2022-09-28 16:51:15 +0200

    model

--- models/campground.js
+++ models/campground.js
@@ -3,10 +3,16 @@ const Schema = mongoose.Schema;
 
 const CampGroundSchema = new Schema({
   title: String,
-  image:String,
+  image: String,
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", CampGroundSchema);
