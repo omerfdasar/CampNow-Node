31a8c0f04b15273a9904a7275a796f1f023eda65
Omer Dasar <omerfarukdasar@gmail.com>
2022-10-02 14:20:56 +0200

    adding review feature

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const Campground = require("./models/campground");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const catchAsync = require("./utils/catchAsync");
-const {campgroundSchema} = require("./schemas")
+const { campgroundSchema, reviewSchema } = require("./schemas");
+const Review = require("./models/review");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,9 +29,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 const validateCampground = (req, res, next) => {
-
   const { error } = campgroundSchema.validate(req.body);
 
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(msg, 400);
@@ -68,7 +78,8 @@ app.post(
 app.get(
   "/campgrounds/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground);
     res.render("campgrounds/show", { campground });
   })
 );
@@ -98,6 +109,19 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+
+app.post(
+  "/campgrounds/:id/reviews",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
