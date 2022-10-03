804eaf6c1728f7db87d7814830fbb1eee6440b26
Omer Dasar <omerfarukdasar@gmail.com>
2022-10-03 19:27:29 +0200

    review styles

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const Review = require('./review')
 const CampGroundSchema = new Schema({
   title: String,
   image: String,
@@ -13,6 +13,16 @@ const CampGroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+CampGroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampGroundSchema);
